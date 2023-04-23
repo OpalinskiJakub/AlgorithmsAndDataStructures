d6f112f76a5fb61e23f22111f30063ea78d76509
Jakub Opalinski <jakub.opalinski@student.uj.edu.pl>
2023-04-23 21:46:09 +0200

    graph and set implementation added

--- src/main/java/org/example/GraphAdjacencyList/Graph.java
+++ src/main/java/org/example/GraphAdjacencyList/Graph.java
@@ -1,4 +1,50 @@
 package org.example.GraphAdjacencyList;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Graph {
+
+    private HashMap<Integer,LinkedList<Integer>> graph = new HashMap();
+
+    private HashMap<Integer,Boolean> visited;
+
+    public void insertVertice(int value){
+        LinkedList<Integer> newVertice = new LinkedList<>();
+        graph.put(value,newVertice);
+    }
+
+    public boolean insertEdge(int firstVertice, int secondVertice){
+        if(graph.get(firstVertice).contains(secondVertice)){
+            return false;
+        }else {
+            graph.get(firstVertice).add(secondVertice);
+            return true;
+        }
+    }
+
+    public boolean checkEdge(int firstVertice, int secondVertice){
+        LinkedList<Integer> list = graph.get(firstVertice);
+        for(int i : list){
+            if(i==secondVertice){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int amountOfEdges(int vertice){
+        return graph.get(vertice).size();
+    }
+
+
+    public static void main(String[] args) {
+        Graph graph = new Graph();
+        graph.insertVertice(1);
+        graph.insertVertice(2);
+        graph.insertEdge(2,1);
+        System.out.println(graph.checkEdge(2,1));
+
+    }
 }
--- src/main/java/org/example/SetTable/Set.java
+++ src/main/java/org/example/SetTable/Set.java
@@ -1,4 +1,65 @@
 package org.example.SetTable;
 
 public class Set {
+
+    private int[] elements;
+
+    private final int lowerLimit;
+
+    private final int upperLimit;
+    public Set(int lowerLimit, int upperLimit){
+        this.lowerLimit = lowerLimit;
+        this.upperLimit = upperLimit;
+        elements = new int[upperLimit-lowerLimit+1];
+    }
+
+    private boolean isInLimit(int value){
+        if((value<lowerLimit)||(value>upperLimit)){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    public boolean insert(int value){
+        if(isInLimit(value)){
+            elements[value - lowerLimit] = 1;
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public boolean withDraw(int value){
+        if(isInLimit(value)){
+            elements[value - lowerLimit] = 0;
+            return true;
+        }else {
+            return false;
+        }
+    }
+    public boolean isInSet(int value){
+        if(isInLimit(value)){
+            if(elements[value-lowerLimit]==1){
+                return true;
+            }else {
+                return false;
+            }
+        }else {
+            return false;
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder =  new StringBuilder();
+        for(int i=0;i<elements.length;i++){
+            if(elements[i]==1) {
+                stringBuilder.append(Integer.toString(i + lowerLimit));
+                stringBuilder.append(" ");
+            }
+        }
+        return stringBuilder.toString();
+    }
+
 }
